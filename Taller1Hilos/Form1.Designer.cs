435f0ba2e342d84c06c3a43d46b3e0b390a7d6b4
gersandres <gersandres@gmail.com>
2016-04-21 19:24:14 -0500

    Primer Commit

---
+++ Taller1Hilos/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Taller1Hilos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.valorText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAcumulado = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // valorText
+            // 
+            this.valorText.Location = new System.Drawing.Point(-3, 454);
+            this.valorText.Name = "valorText";
+            this.valorText.Size = new System.Drawing.Size(100, 20);
+            this.valorText.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-3, 438);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Monto a apostar:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 438);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Monto Acomulado";
+            // 
+            // txtAcumulado
+            // 
+            this.txtAcumulado.Location = new System.Drawing.Point(103, 454);
+            this.txtAcumulado.Name = "txtAcumulado";
+            this.txtAcumulado.Size = new System.Drawing.Size(100, 20);
+            this.txtAcumulado.TabIndex = 4;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(0, 50);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(23, 23);
+            this.btn1.TabIndex = 5;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(0, 149);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(23, 23);
+            this.btn2.TabIndex = 6;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(0, 250);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(23, 23);
+            this.btn3.TabIndex = 7;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(0, 350);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(23, 23);
+            this.btn4.TabIndex = 8;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImage = global::Taller1Hilos.Properties.Resources.fcaballos;
+            this.ClientSize = new System.Drawing.Size(1135, 481);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtAcumulado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.valorText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox valorText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAcumulado;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+    }
+}
+
---
+++ Taller1Hilos/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Taller1Hilos
+{
+    public partial class Form1 : Form
+    {
+
+        private int posx1;
+        private int posx2;
+        private int posx3;
+        private int posx4;
+
+        private int posy1;       
+        private int posy2;
+        private int posy3;
+        private int posy4;
+
+        private int ancho;
+        private int alto;
+
+        Thread hilo1;
+        Thread hilo2;
+        Thread hilo3;
+        Thread hilo4;
+
+        private Position posicion;
+        private Position posicion2;
+        private Position posicion3;
+        private Position posicion4;
+
+        private int ganador;
+
+        public int aux =1000;
+        public int acomulado = 1000;
+
+        Random aletorio = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            txtAcumulado.Text = acomulado + "";
+            valorText.Text = aux + "";
+
+            posy1 = 25;
+            posy2 = 125;
+            posy3 = 225;
+            posy4 = 325;
+
+            ancho = 100;
+            alto = 100;
+          
+            posicion = Position.down;
+            posicion2 = Position.down;
+            posicion3 = Position.down;
+            posicion4 = Position.down;
+
+
+            hilo1 = new Thread(movCaballo1);
+            hilo2 = new Thread(movCaballo2);
+            hilo3 = new Thread(movCaballo3);
+            hilo4 = new Thread(movCaballo4);
+
+            ganador = 0;
+        }
+
+        private void posXInicial()
+        {
+            posx1 = 10;
+            posx2 = 10;
+            posx3 = 10;
+            posx4 = 10;
+        }
+        enum Position
+        {
+            left, right, up, down
+        }
+
+        private void movCaballo1()
+        {
+            while (true)
+            {
+                if (posicion == Position.down && posx1 > 0)
+                {
+                    posx1 = posx1 + aletorio.Next(15);
+                    if (posx1 > 1000 && ganador == 1 && posx1 > posx2 && posx1 > posx3 && posx1 > posx4)
+                    {  
+                        acomulado = acomulado + aux;
+                        hilo2.Suspend();
+                        hilo3.Suspend();
+                        hilo4.Suspend();
+                        MessageBox.Show(" WINNER HORSE 1 Tu saldo es: " + acomulado);
+                        posXInicial();
+                        hilo1.Suspend();
+                    }
+                    else if (posx2 > 1000 || posx3 > 1000 || posx4 > 1000)
+                    {
+                        MessageBox.Show(" Perdiste ");
+                        hilo1.Suspend();
+                        hilo2.Suspend();
+                        hilo3.Suspend();
+                        hilo4.Suspend();
+                    }
+
+                }
+                Invalidate();
+                Thread.Sleep(75);
+            }
+            }
+
+        private void movCaballo2()
+        {
+            while (true)
+            {
+                if (posicion2 == Position.down && posx2 > 0)
+                {
+                    posx2 = posx2 + aletorio.Next(15);
+                    if (posx2 > 1000 && ganador == 2 && posx2 > posx1 && posx2 > posx3 && posx2 > posx4)
+                    {
+                        hilo1.Suspend();
+                        hilo3.Suspend();
+                        hilo4.Suspend();
+                        acomulado = acomulado + aux;
+                        MessageBox.Show(" WINNER HORSE 2 Tu saldo es: " + acomulado);
+                        posXInicial();
+                        hilo2.Suspend();
+                    }
+                    else if (posx1 > 1000 || posx3 > 1000 || posx4 > 1000)
+                    {
+                        MessageBox.Show(" Perdiste ");
+                        hilo2.Suspend();
+                        hilo1.Suspend();
+                        hilo3.Suspend();
+                        hilo4.Suspend();
+                    }
+                }
+                Invalidate();
+                Thread.Sleep(75);
+            }
+        }
+
+
+        private void movCaballo3()
+        {
+            while (true)
+            {
+                if (posicion3 == Position.down && posx3 > 0)
+                {
+                    posx3 = posx3 + aletorio.Next(15);
+                    if (posx3 > 1000 && ganador == 3 && posx3 > posx1 && posx3 > posx2 && posx3 > posx4)
+                    {
+                        hilo1.Suspend();
+                        hilo2.Suspend();
+                        hilo4.Suspend();
+                        acomulado = acomulado + aux;
+                        MessageBox.Show(" WINNER HORSE 3 Tu saldo es: " + acomulado);
+                        posXInicial();
+                        hilo3.Suspend();
+                    }
+                    else if (posx1 > 1000 || posx2 > 1000 || posx4 > 1000)
+                    {
+                        MessageBox.Show(" Perdiste ");
+                        hilo3.Suspend();
+                        hilo1.Suspend();
+                        hilo2.Suspend();
+                        hilo4.Suspend();
+                    }
+                }
+                Invalidate();
+                Thread.Sleep(75);
+            }
+        }
+
+        private void movCaballo4()
+        {
+            while (true)
+            {
+                if (posicion4 == Position.down && posx4 > 0)
+                {
+                    posx4 = posx4 + aletorio.Next(15);
+                    if (posx4 > 1000 && ganador == 4 && posx4 > posx1 && posx4 > posx2 && posx4 > posx3)
+                    {
+                        hilo1.Suspend();
+                        hilo2.Suspend();
+                        hilo3.Suspend();
+                        acomulado = acomulado + aux;
+                        MessageBox.Show(" WINNER HORSE 4 Tu saldo es: " + acomulado);                        
+                        posXInicial();
+                        hilo4.Suspend();
+                    }
+                    else if (posx1 > 1000 || posx2 > 1000 || posx3 > 1000)
+                    {
+                        MessageBox.Show(" Perdiste");
+                        hilo4.Suspend();
+                        hilo1.Suspend();
+                        hilo2.Suspend();
+                        hilo3.Suspend();
+                    }
+                }
+                Invalidate();
+                Thread.Sleep(75);
+            }
+        }
+
+
+        private void correrHilos()
+        {
+            if (acomulado >= aux)
+            {
+                hilo1.Start();
+                hilo2.Start();
+                hilo3.Start();
+                hilo4.Start();
+            }
+            else
+            {
+                MessageBox.Show("No tiene saldo suficiente, Ingrese un monto que no supere " + acomulado);
+            }
+        }
+
+
+        private void definirGanador()
+        {
+           acomulado = acomulado - aux;
+           MessageBox.Show("Perdiste, ahora tu saldo es " + acomulado);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(new Bitmap("caballo1.png"), posx1, posy1, ancho, alto);
+            e.Graphics.DrawImage(new Bitmap("caballo2.png"), posx2, posy2, ancho, alto);
+            e.Graphics.DrawImage(new Bitmap("caballo3.png"), posx3, posy3, ancho, alto);
+            e.Graphics.DrawImage(new Bitmap("caballo4.png"), posx4, posy4, ancho, alto);
+            txtAcumulado.Text = acomulado + "";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            ganador = 1;
+            posXInicial();
+            aux = Convert.ToInt16(valorText.Text);
+            correrHilos();
+         }
+       
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            ganador = 2;
+            posXInicial();
+            aux = Convert.ToInt16(valorText.Text);
+            correrHilos();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            ganador = 3;
+            posXInicial();
+            aux = Convert.ToInt16(valorText.Text);
+            correrHilos();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            ganador = 4;
+            posXInicial();
+            aux = Convert.ToInt16(valorText.Text);
+            correrHilos();
+        }
+    }
+}
